916024edb900819035cd12cd202f18dd3e3101d0
hscottvo <hscottvo@gmail.com>
2021-05-09 17:20:23 -0700

    added clock

--- source/main.c
+++ source/main.c
@@ -8,9 +8,50 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+
+volatile unsigned char TimerFlag = 0;
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn() {
+    TCCR1B = 0x0B;
+
+    OCR1A = 125;
+
+    TIMSK1 = 0x02;
+
+    TCNT1 = 0;
+
+    _avr_timer_cntcurr = _avr_timer_M;
+
+    SREG |= 0x80;
+}
+
+void TimerOff() {
+    TCCR1B = 0x00;
+}
+
+void TimerISR() {
+    TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect) {
+    _avr_timer_cntcurr--;
+    if(_avr_timer_cntcurr == 0) {
+        TimerISR();
+        _avr_timer_cntcurr = _avr_timer_M;
+    }
+}
+
+void TimerSet(unsigned long M) {
+    _avr_timer_M = M;
+    _avr_timer_cntcurr = _avr_timer_M;
+}
 
 void set_PWM(double frequency) {
     static double current_frequency;
@@ -111,11 +152,17 @@ int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
+    TimerSet(250);
+    TimerOn();
     PWM_on();
+    mel_state = mel_wait;
+    play_state = play_wait;
     /* Insert your solution below */
     while (1) {
+        while(!TimerFlag);
         mel_tick();
-        play_tick;
+        play_tick();
+        TimerFlag = 0;
     }
     return 1;
 }
